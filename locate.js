c53bc4a9073f20d9d612d262f001076aba030c69
vedu mallela <vedu.mallela@gmail.com>
2020-06-26 14:39:34 -0400

    initial commit

---
+++ locate.js
@@ -0,0 +1,27 @@
+var long = 0
+var lat = 0
+
+window.onload = function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        alert("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    lat = position.coords.latitude
+    long = position.coords.longitude
+    var map = new ol.Map({
+        target: 'map',
+        layers: [
+            new ol.layer.Tile({
+                source: new ol.source.OSM()
+            })
+        ],
+        view: new ol.View({
+            center: ol.proj.fromLonLat([long, lat]),
+            zoom: 10
+        })
+    });
+}
