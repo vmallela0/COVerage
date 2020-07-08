48d1708a186eed7f2157646e8b9878472848c551
vedu mallela <vedu.mallela@gmail.com>
2020-07-08 19:21:16 -0400

    h1 update

--- scraper.py
+++ scraper.py
@@ -2,25 +2,29 @@ import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
 
-URL = 'https://www.fox5atlanta.com/news/atlanta-mayor-to-sign-order-mandating-masks-to-fight-covid-19'
+URL = 'https://www.mercurynews.com/2020/07/07/h-1b-san-jose-firm-will-pay-to-resolve-claim-it-favored-indian-applicants-over-white-applicants/'
 
 r = requests.get(URL)
 type(r)
 html = r.text
 
 # Create a BeautifulSoup object from the HTML
+
+
 try:
-   page = urlopen(URL)
+    page = urlopen(URL)
 except:
-   print("Error opening the URL")
+    print("page not working")
 
 soup = BeautifulSoup(page, 'html.parser')
-content = soup.find('div', {"class": "article-body"})
+content = soup.find('div') # , {"class": "article-body"}
+header = soup.find('h1')
 
 article = ''
 for i in content.findAll('p'):
     article = article + ' ' +  i.text
 
+print(header)
 print(article)
 
 # print(soup.p)
