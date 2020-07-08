cb7c2c99ef139afb0b9e4064e6bcf2d06f997ae7
AaditT <aadit.trivedi03@gmail.com>
2020-07-08 16:37:48 -0700

    fixed header

--- scraper.py
+++ scraper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 from urllib.request import urlopen
-
+import json
 URL = 'https://www.mercurynews.com/2020/07/07/h-1b-san-jose-firm-will-pay-to-resolve-claim-it-favored-indian-applicants-over-white-applicants/'
 
 r = requests.get(URL)
@@ -18,13 +18,24 @@ except:
 
 soup = BeautifulSoup(page, 'html.parser')
 content = soup.find('div') # , {"class": "article-body"}
-header = soup.find('h1')
+header = soup.find('h1').get_text().strip()
+#print(content)
 
 article = ''
 for i in content.findAll('p'):
     article = article + ' ' +  i.text
 
 print(header)
-print(article)
+
+def write_json(data, filename='corpus.json'):
+    with open(filename,'w') as f:
+        json.dump(data, f, indent=4)
+        
+new_data = {str(header) : str(article)}
+with open('corpus.json') as json_file:
+    data = json.load(json_file)
+    corpus = data['corpus']
+    corpus.append(new_data)
+write_json(data)
 
 # print(soup.p)
