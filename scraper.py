e827127e4e08d8b260e015dc345885b611191e52
AaditT <aadit.trivedi03@gmail.com>
2020-07-08 16:07:38 -0700

    Fox news scraper

--- scraper.py
+++ scraper.py
@@ -1,11 +1,26 @@
 import requests
 from bs4 import BeautifulSoup
+from urllib.request import urlopen
 
 URL = 'https://www.fox5atlanta.com/news/atlanta-mayor-to-sign-order-mandating-masks-to-fight-covid-19'
-page = requests.get(URL)
 
-soup = BeautifulSoup(page.content, 'html.parser')
-headline = soup.find("h1")
-content = soup.find("article")
-print(headline.prettify())
-print(content.prettify())
+r = requests.get(URL)
+type(r)
+html = r.text
+
+# Create a BeautifulSoup object from the HTML
+try:
+   page = urlopen(URL)
+except:
+   print("Error opening the URL")
+
+soup = BeautifulSoup(page, 'html.parser')
+content = soup.find('div', {"class": "article-body"})
+
+article = ''
+for i in content.findAll('p'):
+    article = article + ' ' +  i.text
+
+print(article)
+
+# print(soup.p)
