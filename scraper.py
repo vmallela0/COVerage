d6137b1aad93f0345fcc372302cc82b18e7dea51
vedu mallela <vedu.mallela@gmail.com>
2020-07-09 11:28:53 -0400

    added training urls

--- scraper.py
+++ scraper.py
@@ -4,9 +4,7 @@ from urllib.request import urlopen
 import json
 URL = 'https://www.mercurynews.com/2020/07/07/h-1b-san-jose-firm-will-pay-to-resolve-claim-it-favored-indian-applicants-over-white-applicants/'
 
-r = requests.get(URL)
-type(r)
-html = r.text
+
 
 
 
@@ -16,22 +14,54 @@ whitelist = ['\t', '\n']
 
 training_urls = {
     "politics": [
-
+        'https://www.sccgov.org/sites/covid19/Pages/home.aspx',
+        'https://www.cupertino.org/our-city/city-news/coronavirus-covid-19',
+        'https://www.santaclaraca.gov/i-want-to/stay-informed/current-topics/coronavirus-updates',
+        'https://www.cdc.gov/coronavirus/2019-ncov/cases-updates/california-santa-clara-county.html',
+        'https://www.sfchronicle.com/news/article/Coronavirus-FAQ-Should-you-wear-a-face-mask-15147298.php',
+        'https://www.mercurynews.com/2020/05/20/santa-clara-county-now-requires-that-you-wear-a-face-covering-san-jose-wants-to-go-further/',
     ],
     "education": [
-
+        'https://www.cde.ca.gov/ls/he/hn/strongertogether.asp',
+        'https://www.sccoe.org/covid-19/Pages/latest-updates.aspx',
+        'https://www.sfchronicle.com/bayarea/article/Bay-Area-high-school-students-miss-academic-15152197.php',
+        'https://www.mercurynews.com/2020/05/28/california-school-districts-could-forego-attendance-standardized-tests-during-coronavirus-pandemic/',
+        'https://abc7news.com/education/watch-today-state-superintendent-discusses-guidelines-to-reopen-schools-/6201690/'
     ],
     "biology": [
-
+        'https://www.cidrap.umn.edu/news-perspective/2020/05/scientists-exactly-zero-evidence-covid-19-came-lab',
+        'https://www.nature.com/articles/d41586-020-01449-8',
+        'https://www.cdc.gov/coronavirus/2019-ncov/prevent-getting-sick/prevention.html#:~:text=The%20virus%20is%20thought%20to,are%20not%20showing%20symptoms.',
+        'https://www.cdc.gov/coronavirus/2019-ncov/daily-life-coping/animals.html',
+        'https://www.raps.org/news-and-articles/news-articles/2020/3/covid-19-vaccine-tracker',
+        'https://www.nytimes.com/interactive/2020/science/coronavirus-vaccine-tracker.html',
+        'https://landing.google.com/screener/covid19',
+        'https://www.sccgov.org/sites/covid19/Pages/covid19-testing.aspx',
+        'https://www.cvs.com/minuteclinic/covid-19-testing'
     ],
     "economy": [
-
+        'https://www.nytimes.com/interactive/2020/us/states-reopen-map-coronavirus.html',
+        'https://www.mercurynews.com/2020/06/12/coronavirus-retail-valley-fair-oakridge-malls-prep-re-openings-economy-commercial-real-estate-jobs/',
+        'https://sanjosespotlight.com/category/coronavirus/',
+        'https://www.sccgov.org/sites/covid19/Pages/business-guidance.aspx',
+        'https://www.sfchronicle.com/business/article/Out-of-work-because-of-coronavirus-These-15135747.php',
+        'https://www.latimes.com/projects/california-coronavirus-cases-tracking-outbreak/unemployment/'
     ],
     "statistics": [
-
+        'https://www.reporternewspapers.net/2020/06/26/june-26-atlanta-sandy-springs-covid-19-report-180-more-diagnoses-14-day-trend-is-up/',
+        'https://www.fox5atlanta.com/news/georgia-coronavirus-cases-are-up-and-its-not-just-because-of-testing-doctors-say',
+        'https://www.11alive.com/article/news/health/coronavirus/coronavirus-numbers/covid-19-in-georgia-numbers-june-24-cases/85-aed1f537-d064-47dd-84fc-bf1b7ac1e483',
+        'https://www.11alive.com/article/news/health/coronavirus/coronavirus-numbers/covid-19-cases-in-atlanta-zip-code/85-d589e7e8-d6ef-4c43-988e-f418a5a1c01a',
+        'https://www.wsbtv.com/news/local/atlanta/atlanta-mayor-says-city-is-experiencing-an-all-time-high-cases/MLZQDVZPCVC6LL6JYBZUMJK4BQ/',
+        'https://wgxa.tv/news/coronavirus/hospitalizations-rise-as-georgia-virus-cases-hit-new-peak',
+        'https://dph.georgia.gov/covid-19-daily-status-report'
     ],
 }
 
+
+r = requests.get(URL)
+type(r)
+html = r.text
 
 try:
     page = urlopen(URL)
