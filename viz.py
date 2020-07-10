4710cd2c523c5c1cc9eb8a98a7fc80cd4d975465
AaditT <aadit.trivedi03@gmail.com>
2020-07-10 14:32:56 -0700

    return

--- viz.py
+++ viz.py
@@ -2,4 +2,4 @@ from nltk.corpus import stopwords
 def return_words(summary):
     split_sentences = summary.split()
     filtered_words = [word for word in split_sentences if word not in stopwords.words('english')]
-    print(filtered_words)
+    return filtered_words
