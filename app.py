9ebcf2b40738adf52a56f8f4f9501ea14ab8b697
AaditT <aadit.trivedi03@gmail.com>
2020-07-21 21:23:10 -0700

    fixed all links

--- app.py
+++ app.py
@@ -2,12 +2,16 @@ from flask import *
 app = Flask(__name__)
 
 @app.route('/')
-def hello_world():
+def home():
     return render_template("index.html")
 
 @app.route('/about')
 def about():
     return render_template("about.html")
+
+@app.route('/donate')
+def donate():
+    return render_template("donate.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
