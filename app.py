fab6fd14081db9e7773e3d7798692867316adc04
AaditT <aadit.trivedi03@gmail.com>
2020-07-21 21:17:56 -0700

    fixed links pt 1

--- app.py
+++ app.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def hello_world():
     return render_template("index.html")
 
+@app.route('/about')
+def about():
+    return render_template("about.html")
 
 if __name__ == '__main__':
     app.run(debug=True, port=5000)
