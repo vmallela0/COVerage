10686c6b8afb9f2bc6937fe1c2e55f1e3fcd2564
vedu mallela <vedu.mallela@gmail.com>
2020-07-25 22:48:58 -0400

    mdae it easier to declare defaults

--- data_backend.py
+++ data_backend.py
@@ -1,63 +1,27 @@
-headline_policies_1 = "Policies 1"
-headline_policies_2 = "Policies 2"
-headline_policies_3 = "Policies 3"
-headline_policies_4 = "Policies 4"
-headline_policies_5 = "Policies 5"
+# data for index
 
-headline_education_1 = "Education 1"
-headline_education_2 = "Education 2"
-headline_education_3 = "Education 3"
-headline_education_4 = "Education 4"
-headline_education_5 = "Education 5"
+# declaring default values
+headline_policies_1 = headline_policies_2 = headline_policies_3 = headline_policies_4 = headline_policies_5 = "Policies 5"
 
-headline_biology_1 = "Biology 1"
-headline_biology_2 = "Biology 2"
-headline_biology_3 = "Biology 3"
-headline_biology_4 = "Biology 4"
-headline_biology_5 = "Biology 5"
+headline_education_1 = headline_education_2 = headline_education_3 = headline_education_4 = headline_education_5 = "Education 5"
 
-headline_economy_1 = "Economy 1"
-headline_economy_2 = "Economy 2"
-headline_economy_3 = "Economy 3"
-headline_economy_4 = "Economy 4"
-headline_economy_5 = "Economy 5"
+headline_biology_1 = headline_biology_2 = headline_biology_3 = headline_biology_4 = headline_biology_5 = "Biology 5"
 
-headline_statistics_1 = "Statistics 1"
-headline_statistics_2 = "Statistics 2"
-headline_statistics_3 = "Statistics 3"
-headline_statistics_4 = "Statistics 4"
-headline_statistics_5 = "Statistics 5"
+headline_economy_1 = headline_economy_2 = headline_economy_3 = headline_economy_4 = headline_economy_5 = "Economy 5"
+
+headline_statistics_1 = headline_statistics_2 = headline_statistics_3 = headline_statistics_4 = headline_statistics_5 = "Statistics 5"
 
 # Paragraphs
-text_policies_1 = "text about policies"
-text_policies_2 = "text about policies"
-text_policies_3 = "text about policies"
-text_policies_4 = "text about policies"
-text_policies_5 = "text about policies"
+text_policies_1 = text_policies_2 = text_policies_3 = text_policies_4 = text_policies_5 = "text about policies"
 
-text_education_1 = "text about education"
-text_education_2 = "text about education"
-text_education_3 = "text about education"
-text_education_4 = "text about education"
-text_education_5 = "text about education"
+text_education_1 = text_education_2 = text_education_3 = text_education_4 = text_education_5 = "text about education"
 
-text_biology_1 = "text about biology"
-text_biology_2 = "text about biology"
-text_biology_3 = "text about biology"
-text_biology_4 = "text about biology"
-text_biology_5 = "text about biology"
+text_biology_1 = text_biology_2 = text_biology_3 = text_biology_4 = text_biology_5 = "text about biology"
 
-text_economy_1 = "text about economy"
-text_economy_2 = "text about economy"
-text_economy_3 = "text about economy"
-text_economy_4 = "text about economy"
-text_economy_5 = "text about economy"
+text_economy_1 = text_economy_2 = text_economy_3 = text_economy_4 = text_economy_5 = "text about economy"
 
-text_statistics_1 = "text about statistics"
-text_statistics_2 = "text about statistics"
-text_statistics_3 = "text about statistics"
-text_statistics_4 = "text about statistics"
-text_statistics_5 = "text about statistics"
+text_statistics_1 = text_statistics_2 = text_statistics_3 = text_statistics_4 = text_statistics_5 = "text about statistics"
+
 
 lavaa = {
     "policies":
