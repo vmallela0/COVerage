226b2b0f9acdc718542b0461113e638a41ab9ffc
vedu mallela <vedu.mallela@gmail.com>
2020-07-26 02:43:36 -0400

    added images to flask/data_backend

--- data_backend.py
+++ data_backend.py
@@ -1,15 +1,15 @@
 # data for index
 
 # declaring default values
-headline_policies_1 = headline_policies_2 = headline_policies_3 = headline_policies_4 = headline_policies_5 = "Policies 5"
+headline_policies_1 = headline_policies_2 = headline_policies_3 = headline_policies_4 = headline_policies_5 = "Policies default"
 
-headline_education_1 = headline_education_2 = headline_education_3 = headline_education_4 = headline_education_5 = "Education 5"
+headline_education_1 = headline_education_2 = headline_education_3 = headline_education_4 = headline_education_5 = "Education default"
 
-headline_biology_1 = headline_biology_2 = headline_biology_3 = headline_biology_4 = headline_biology_5 = "Biology 5"
+headline_biology_1 = headline_biology_2 = headline_biology_3 = headline_biology_4 = headline_biology_5 = "Bio default"
 
-headline_economy_1 = headline_economy_2 = headline_economy_3 = headline_economy_4 = headline_economy_5 = "Economy 5"
+headline_economy_1 = headline_economy_2 = headline_economy_3 = headline_economy_4 = headline_economy_5 = "Economy default"
 
-headline_statistics_1 = headline_statistics_2 = headline_statistics_3 = headline_statistics_4 = headline_statistics_5 = "Statistics 5"
+headline_statistics_1 = headline_statistics_2 = headline_statistics_3 = headline_statistics_4 = headline_statistics_5 = "Stat default"
 
 # Paragraphs
 text_policies_1 = text_policies_2 = text_policies_3 = text_policies_4 = text_policies_5 = "text about policies"
@@ -22,10 +22,27 @@ text_economy_1 = text_economy_2 = text_economy_3 = text_economy_4 = text_economy
 
 text_statistics_1 = text_statistics_2 = text_statistics_3 = text_statistics_4 = text_statistics_5 = "text about statistics"
 
+#images
+img_policies_1 = img_policies_2 = img_policies_3 = img_policies_4 = img_policies_5 = "https://imgur.com/lNhUEmK.png"
+
+img_education_1 = img_education_2 = img_education_3 = img_education_4 = img_education_5 = "https://imgur.com/KiDWiTr.png"
+
+img_biology_1 = img_biology_2 = img_biology_3 = img_biology_4 = img_biology_5 = "https://imgur.com/aY2TasC.png"
+
+img_economy_1 = img_economy_2 = img_economy_3 = img_economy_4 = img_economy_5 = "https://imgur.com/W9kLJHc.png"
+
+img_statistics_1 = img_statistics_2 = img_statistics_3 = img_statistics_4 = img_statistics_5 = "https://imgur.com/8IzNJTT.png"
 
 lavaa = {
     "policies":
         {
+            "image": [
+                img_policies_1,
+                img_policies_2,
+                img_policies_3,
+                img_policies_4,
+                img_policies_5
+            ],
             "text": [
                 text_policies_1,
                 text_policies_2,
@@ -44,6 +61,13 @@ lavaa = {
         },
     "education":
         {
+            "image": [
+                img_education_1,
+                img_education_2,
+                img_education_3,
+                img_education_4,
+                img_education_5
+            ],
             "text": [
                 text_education_1,
                 text_education_2,
@@ -62,6 +86,13 @@ lavaa = {
         },
     "biology":
         {
+            "image": [
+                img_biology_1,
+                img_biology_2,
+                img_biology_3,
+                img_biology_4,
+                img_biology_5
+            ],
             "text": [
                 text_biology_1,
                 text_biology_2,
@@ -81,6 +112,13 @@ lavaa = {
         },
     "economy":
         {
+            "image": [
+                img_economy_1,
+                img_economy_2,
+                img_economy_3,
+                img_economy_4,
+                img_economy_5
+            ],
             "text": [
                 text_economy_1,
                 text_economy_2,
@@ -99,6 +137,13 @@ lavaa = {
         },
     "statistics":
         {
+            "image": [
+                img_statistics_1,
+                img_statistics_2,
+                img_statistics_3,
+                img_statistics_4,
+                img_statistics_5
+            ],
             "text": [
                 text_statistics_1,
                 text_statistics_2,
