ad8641f0fe23be9c08c1fe2a125533b25d8adcaf
vedu mallela <vedu.mallela@gmail.com>
2020-07-27 12:56:55 -0400

    export_searchinator

--- searcher.py
+++ searcher.py
@@ -13,4 +13,18 @@ def searcher(county, category):
         urls.append(url)
     return urls
 
-print(searcher("Santa Clara County", "Economy")) # testing
+cat_list = ["policies", "education", "biology", "economy", "statistics"]
+search_export = {}
+for i in cat_list:
+    search_export[i] = []
+
+def export_search(county, cats, search_dict):
+    for i in cats:
+        search_dict[i].append(searcher(county, i))
+    return search_dict
+
+print(export_search("Santa Clara County, CA", cat_list, search_export)['policies'])
+print(export_search("Santa Clara County, CA", cat_list, search_export)['education'])
+print(export_search("Santa Clara County, CA", cat_list, search_export)['biology'])
+print(export_search("Santa Clara County, CA", cat_list, search_export)['economy'])
+print(export_search("Santa Clara County, CA", cat_list, search_export)['statistics'])
