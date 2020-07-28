0cdebc27e4d63b1c1f0e85703b40304babfbae25
vedu mallela <vedu.mallela@gmail.com>
2020-07-27 22:19:56 -0400

    they're 404's. They don't do much

--- app.py
+++ app.py
@@ -25,6 +25,10 @@ def home():
 def about():
     return render_template("about.html")
 
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template('404.html'), 404
+
 @app.route('/donate')
 def donate():
     return render_template("donate.html")
